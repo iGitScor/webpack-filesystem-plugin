a2cf404ede3ca37713e08cdd261bfcacdb60cbbb
Sebastien Correaud <sebastien.correaud@gmail.com>
2017-06-12 08:34:31 +0200

    Add log level to handle crash (#15)
    * Add log level to handle crash
    
    * Downgrade eslint

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const _ = require('lodash');
 const fs = require('fs');
 const childProcess = require('child_process');
+const logger = require('./helpers/logger');
 
 const validAction = ['cp', 'rm'];
 const validBuildTrigger = ['after-emit', 'done', 'failed'];
@@ -16,13 +17,30 @@ module.exports = class Filesystem {
       this.buildTrigger = options.buildTrigger || 'after-emit';
       this.silent = options.silent || true;
       this.verbose = !this.silent;
+
+      this.logLevel = options.logLevel || 'strict';
+      this.addLogger();
     } else {
       // Throw exception
       const error = typeof options === 'object'
         ? 'Required parameters are missing'
         : 'Parameters are invalid';
 
+      this.silent = false;
+      this.logLevel = 'strict';
+      this.addLogger();
+      this.exceptionLogger.warn(error);
+
       throw new Error(error);
+    }
+  }
+
+  addLogger() {
+    if (!this.silent) {
+      this.exceptionLogger = logger(this.logLevel);
+    } else {
+      // Empty logger when silent mode is activated
+      this.exceptionLogger = logger();
     }
   }
 
@@ -49,14 +67,20 @@ module.exports = class Filesystem {
       const spawn = childProcess.spawnSync(this.action, _.without(argumentsArray, null));
       const errorText = spawn.stderr.toString().trim();
 
-      if (errorText) {
+      if (errorText && this.logLevel === 'strict') {
         throw new Error('Command failed');
       }
 
       return true;
     }
 
-    throw new Error('File not found');
+    this.exceptionLogger.warn('File not found');
+
+    if (this.logLevel === 'strict') {
+      throw new Error('File not found');
+    }
+
+    return false;
   }
 
   apply(compiler) {
